bb58b40fe66d3c1716a9325b882250ebb0a81147
unknown <admin@SKM-2716.skm.org.pk>
2026-01-27 23:41:15 +0500

    Railway deployment config

--- src/main/java/com/tricure/backend/controller/CmsController.java
+++ src/main/java/com/tricure/backend/controller/CmsController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 public class CmsController {
 
     @Autowired private ServiceRepository serviceRepository;
