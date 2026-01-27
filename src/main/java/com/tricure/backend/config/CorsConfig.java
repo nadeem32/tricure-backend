fb3a900c726062a53caae87fb09247cd54b23699
unknown <admin@SKM-2716.skm.org.pk>
2026-01-27 23:27:26 +0500

    Railway deployment config

--- src/main/java/com/tricure/backend/config/CorsConfig.java
+++ src/main/java/com/tricure/backend/config/CorsConfig.java
@@ -1,27 +1,18 @@
 package com.tricure.backend.config;
 
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-            	registry.addMapping("/**")
-                .allowedOrigins("https://tricure-frontend-production.up.railway.app")
-//                https://tricure-frontend-production.up.railway.app
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-            	 .allowCredentials(true);
-
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://tricure-frontend-production.up.railway.app")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
     }
 }
