a7d4d53b11f083cfbe34f5216c7c3f39a90d2bc9
nadeem32 <51081511+nadeem32@users.noreply.github.com>
2026-03-02 20:09:04 +0500

    Update allowed origins in CORS configuration

--- src/main/java/com/tricure/backend/config/CorsConfig.java
+++ src/main/java/com/tricure/backend/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("https://tricure-frontend-production.up.railway.app")
+            .allowedOrigins("https://tricure-frontend-production-8a00.up.railway.app")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*");
 //            .allowCredentials(true);
